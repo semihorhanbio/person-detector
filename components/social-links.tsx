0e0b9a0c3f8f78cbbbd246644075e6a2e2b422fd
Semih <50242428+semihorhanbio@users.noreply.github.com>
2024-02-16 13:02:08 +0300

    Update social links

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -1,17 +1,14 @@
 'use client'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faYoutube, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const SocialMediaLinks = () => {
     return (
         <div className='flex flex-row gap-4'>
-            <a href="https://github.com/KoushikJit" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/semihorhanbio" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faGithub} size="2x" />
             </a>
-            <a href="https://www.youtube.com/channel/UCfKRZUU78XIcQvh1_QsdQyA" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faYoutube} size="2x" />
-            </a>
-            <a href="https://www.linkedin.com/in/koushik-sarkar-iimc/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/semihorhan/" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faLinkedin} size="2x" />
             </a>
         </div>
